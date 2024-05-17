0db9f57c13a2b04e7495ddd31accaa299748a994
Gowtham P <143248560+GowthamP04@users.noreply.github.com>
2024-05-17 18:02:22 +0530

    first commit

---
+++ src/test/java/test/Cricbuzz.java
@@ -0,0 +1,22 @@
+package test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Cricbuzz {
+
+	@Test
+	public void cricbuzz() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.cricbuzz.com/");
+		String title=driver.getTitle();
+		String url=driver.getCurrentUrl();
+		System.out.println("title of page"+title);
+		System.out.println("title of url"+url);
+		driver.quit();
+	}
+}
