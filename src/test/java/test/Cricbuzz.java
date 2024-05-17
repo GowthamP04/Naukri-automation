fddf9ca0795c53011790ccdd7f54c92da782eb19
Gowtham P <143248560+GowthamP04@users.noreply.github.com>
2024-05-17 18:18:10 +0530

    Update Cricbuzz.java

--- src/test/java/test/Cricbuzz.java
+++ src/test/java/test/Cricbuzz.java
@@ -10,13 +10,17 @@ public class Cricbuzz {
 	@Test
 	public void cricbuzz() {
 		
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.cricbuzz.com/");
-		String title=driver.getTitle();
-		String url=driver.getCurrentUrl();
-		System.out.println("title of page"+title);
-		System.out.println("title of url"+url);
-		driver.quit();
+		// WebDriverManager.chromedriver().setup();
+		// WebDriver driver=new ChromeDriver();
+		// driver.get("https://www.cricbuzz.com/");
+		// String title=driver.getTitle();
+		// String url=driver.getCurrentUrl();
+		// System.out.println("title of page"+title);
+		// System.out.println("title of url"+url);
+		// driver.quit();
+		 System.out.println("--------------------------------------------");
+ System.out.println("---------------------gowtham-----------------------");
+		 System.out.println("--------------------------------------------");
+		
 	}
 }
